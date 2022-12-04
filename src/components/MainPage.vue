dc8e18ee0715adac966e86003ed699c5c160cd93
RamiGuellil <67952392+RamiGuellil@users.noreply.github.com>
2022-12-04 22:27:50 +0100

    dashboard

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,20 @@
 <template>
-  <NavBar v-model="isShow"/>
+  <NavBar v-model="isShow" />
   <v-app-bar flat>
-    <v-app-bar-nav-icon @click="changeTemp()" class="d-flex d-lg-none"></v-app-bar-nav-icon>
+    <v-app-bar-nav-icon
+      @click="changeTemp()"
+      class="d-flex d-lg-none"
+    ></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-btn min-width="0" class="app-bar_icon-button" color="#A1A5B7" style="margin-right: 10px">
+      <v-icon size="large">mdi-message</v-icon>
+    </v-btn>
+    <v-btn min-width="0" class="app-bar_icon-button" color="#A1A5B7" style="margin-right: 10px">
+      <v-icon size="large">mdi-bell</v-icon>
+    </v-btn>
+    <v-avatar @click="" color="#A1A5B7" style="margin-right: 5px" class="app-bar_avatar">
+      <v-img :src="Octocat" />
+    </v-avatar>
   </v-app-bar>
   <v-divider></v-divider>
   <v-layout full-height style="background-color: #f4f4f4">
@@ -12,19 +25,36 @@
 </template>
 <script>
 import NavBar from "./NavBar.vue";
-import {ref} from "vue";
+import { ref } from "vue";
+import Octocat from '@/assets/Octocat.png'
 export default {
   components: {
     NavBar,
   },
   setup() {
     const isShow = ref(true);
-    return {isShow};
+    return { isShow };
   },
   methods: {
-    changeTemp(){
-        this.isShow = true;
+    changeTemp() {
+      this.isShow = true;
+    },
+    showMenu(){
+      console.log('I am here');
+    }
+  },
+  computed: {
+    Octocat(){
+      return Octocat;
     }
   }
 };
 </script>
+<style scoped>
+.app-bar_icon-button:hover .v-icon{
+  color: #009ef7 !important;
+}
+.app-bar_avatar{
+  cursor: pointer;
+}
+</style>
