669b2f02e140b5483eeeb200c2bd9a95e2b455eb
RamiGuellil <67952392+RamiGuellil@users.noreply.github.com>
2022-12-10 13:15:17 +0100

    clients

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -19,15 +19,7 @@
               >
                 Another {{ goalToEarned - earned }} to Goal
               </div>
-              <v-sparkline
-                fill
-                :gradient="gradient"
-                line-width="2"
-                padding="8"
-                smooth="10"
-                :value="value"
-                auto-draw
-              ></v-sparkline>
+              <LineChart :chartData="testData" :options="options" />
             </v-card-text>
           </v-card>
         </v-col>
@@ -43,6 +35,19 @@
             <v-col>
               <v-card flat>
                 <v-card-title>Top Clients</v-card-title>
+                <v-card-text>
+                  <v-list lines="two">
+                    <v-list-item v-for="client in clients" :title="client.name"
+        :subtitle="client.address">
+                      <template v-slot:prepend>
+                        <v-avatar color="grey-lighten-1">
+                          <v-img :src="client.photo" cover></v-img>
+                        </v-avatar>
+                      </template>
+                      <v-divider style="margin-top: 1.8em;"></v-divider>
+                    </v-list-item>
+                  </v-list>
+                </v-card-text>
               </v-card>
             </v-col>
           </v-row>
@@ -53,13 +58,93 @@
 </template>
 <script>
 import { ref } from "vue";
+import { LineChart } from "vue-chart-3";
+//import {client_103512102022, client_111312102022, client_113712102022, client_114212102022, client_114412102022} from "@/assets/";
+import client_103512102022 from "@/assets/client_103512102022.jpg";
+import client_111312102022 from "@/assets/client_111312102022.jpg";
+import client_113712102022 from "@/assets/client_113712102022.jpg";
+import client_114212102022 from "@/assets/client_114212102022.jpg";
+import client_114412102022 from "@/assets/client_114412102022.jpg";
 export default {
+  components: {
+    LineChart,
+  },
   setup() {
-    const earned = ref(2000);
+    const earned = ref(3350);
     const goalToEarned = ref(5000);
-    const value= [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0];
-    const gradient = ['#42b3f4'];
-    return { earned, goalToEarned, value, gradient };
+    const testData = {
+      labels: ["17 Jan", "20 Jan", "23 Jan", "27 Jan", "30 Jan"],
+      datasets: [
+        {
+          data: [1200, 500, 750, 300, 600, 1500],
+          fill: true,
+          borderColor: "#2554FF",
+          backgroundColor: "rgba(125,150,220,0.5)",
+          borderWidth: 1,
+          label: "Earning",
+          tension: 0.3,
+          pointStyle: "circle",
+          pointRadius: 5,
+          pointHoverRadius: 10,
+        },
+      ],
+    };
+    const options = {
+      scales: {
+        y: {
+          ticks: {
+            beginAtZero: true,
+          },
+          grid: {
+            display: true,
+            color: "#e2e2e2",
+          },
+        },
+        x: {
+          grid: {
+            display: false,
+          },
+        },
+      },
+      legend: {
+        display: true,
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+    };
+    const clients = [
+      {
+        name: "Steve E. Lane",
+        phone: "+1 xxx-xxx-xxxx",
+        address: "2976 Deer Ridge Drive Summit, NJ 07901",
+        photo: client_103512102022,
+      },
+      {
+        name: "Martha D. Authement",
+        phone: "+1 xxx-xxx-xxxx",
+        address: "297 Tori Lane Salt Lake City, UT 84116",
+        photo: client_111312102022,
+      },
+      {
+        name: "Eric J. Koon",
+        phone: "+1 xxx-xxx-xxxx",
+        address: "2178 Linden Avenue Altamonte Springs, FL 32701",
+        photo: client_113712102022,
+      },
+      {
+        name: "Antonio C. Newton",
+        phone: "+1 xxx-xxx-xxxx",
+        address: "2380 Rainbow Drive Akron, OH 44308",
+        photo: client_114412102022,
+      },
+      {
+        name: "Debra C. Dominguez",
+        phone: "+1 xxx-xxx-xxxx",
+        address: "2336 Drummond Street Morristown, NJ 07960",
+        photo: client_114212102022,
+      },
+    ];
+    return { earned, goalToEarned, testData, options, clients };
   },
 };
 </script>
